aefc93d18249dcfe8aa30a7e867faabcfd3ede11
Adriana Villegas <villegas.a102@gmail.com>
2022-07-14 19:54:15 -0700

    adding Id's to each row and hopefully color code

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 var time = moment().format("MMM Do YYYY");
 $("#currentDay").text(time);
+
+var currentTime = moment().format('LT');
+
+if (currentTime === 8) {
+  setColor .present;
+} else if (currentTime >= 8) {
+  document.body.past;
+} else (currentTime <= 8) {
+  document.body.future;
+}
